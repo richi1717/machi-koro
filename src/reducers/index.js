378c206a519dcd909a3cdd402abda76130b4ba0b
Richi Jeffery <kikki_sawn@yahoo.com>
2016-02-07 02:31:28 -0700

    add reducers for cards

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import CardsReducer from './reducer_cards';
+
+const rootReducer = combineReducers({
+  cards: CardsReducer
+});
+
+export default rootReducer;
