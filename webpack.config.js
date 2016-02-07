49ca1dc6462da3c0f1e268d5b4203ac22160f18c
Richi Jeffery <kikki_sawn@yahoo.com>
2016-02-06 19:36:43 -0700

    initialize machi-koro

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var stylelint = require('stylelint');
+var webpack = require('webpack');
+
+const PATHS = {
+  config: path.join(__dirname, './config'),
+  spec: path.join(__dirname, 'test'),
+  app: path.join(__dirname, 'src'),
+  components: path.join(__dirname, './components'),
+  css: path.join(__dirname, 'scss')
+};
+
+module.exports = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    path.resolve(__dirname, 'src/app.jsx')
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: "inline-source-map",
+  plugins: [
+      new HtmlwebpackPlugin({
+        title: 'Machi Koro',
+        template: 'build/index.html',
+        inject: true
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        "window.jQuery": 'jquery'
+      })
+  ],
+  module: {
+    preLoaders: [
+
+        {test: /\.jsx$/, loader: "eslint-loader", exclude: /node_modules/},
+        {test: /\.scss$/, loaders: ['postcss'], include: PATHS.css}
+
+    ],
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loaders: ["babel-loader"],
+        include: [
+          PATHS.app,
+          PATHS.components
+        ],
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.scss$/,
+         loaders: ["style", "css?sourceMap", "sass?sourceMap"],
+        // loaders: "style-loader!css-loader!sass-loader",
+        include: [
+          PATHS.css,
+          PATHS.components
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.json']
+  },
+  postcss: function () {
+    return [stylelint({
+      extends: [PATHS.config + "/.stylelintrc"],
+      rules: {
+        'color-hex-case': 'lower'
+      }
+    })];
+  },
+  eslint: {
+    configFile: PATHS.config + '/.eslintrc',
+    rules: {
+      "no-var": 0,
+      "no-unused-vars": 0
+    }
+  }
+}
