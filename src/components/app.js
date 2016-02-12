65f793126094b446d2b7b7b340c5a64d975059fb
Richi Jeffery <kikki_sawn@yahoo.com>
2016-02-12 00:19:50 -0700

    update app & card to render properly

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Expansion from '../containers/expansion_buttons';
 import CardsList from '../containers/cards_list';
+import Card from './card.js';
 
 export default class App extends Component {
   render() {
--- src/components/card.js
+++ src/components/card.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { sortCards } from '../actions/index';
 
 export default (props) => {
+  // console.log(props);
   return (
     <div>
       <img src={props.image} />
