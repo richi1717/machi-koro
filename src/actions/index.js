a4ec7b33431c0ce7a04f54208a9c0fa420f0e1e9
Richi Jeffery <kikki_sawn@yahoo.com>
2016-02-14 00:57:27 -0700

    add action to set expansion

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import _ from 'lodash';
+import sortedCards from '../utils/sort';
 
 const ROOT_URL = 'http://localhost:3000';
 // New ES6 injection uses back tick `` with ${} to pass in, very cool!!!
@@ -12,39 +13,15 @@ export function fetchCards(expansion) {
   const url = `${ROOT_URL}/cards?gameSource=${expQuery}&gameSource=Base`;
   const request = axios.get(url)
     .then(function (data){
-      let sortedCards = _.orderBy(data.data, function (obj) {
-        if (obj.roll.length > 0) {
-          return obj.roll[0];
-        } else {
-          return obj.roll;
-        }
-      });
-      sortedCards = _.orderBy(data.data, function (obj) {
-        if (obj.roll.length > 0) {
-          return obj.roll[1];
-        } else {
-          return obj.roll;
-        }
-      });
-      return sortedCards;
+      if (expansion === 'Base') {
+        return sortedCards(data);
+      } else {
+        return data.data;
+      }
     });
 
   return {
     type: FETCH_CARDS,
     payload: request
-  };
-}
-
-export function sortCards(cards) {
-  let sortedCards = _.orderBy(cards, function (obj) {
-    if (obj.roll.length > 0) {
-      return obj.roll[0];
-    } else {
-      return obj.roll;
-    }
-  });
-  console.log(sortedCards);
-  return {
-    payload: sortedCards
   };
 }
