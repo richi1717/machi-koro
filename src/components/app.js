8095f0ddc4bc330f1e8d20553f321fdb342a5a8c
Richi Jeffery <kikki_sawn@yahoo.com>
2016-02-25 09:29:59 -0700

    add functionality for new components

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import Expansion from '../containers/expansion_buttons';
 import CardsList from '../containers/cards_list';
 import Card from './card.js';
+import Dice from '../dice/dice';
+import Coins from '../coins/coins';
+import Turn from '../containers/turn';
 
 export default class App extends Component {
   render() {
@@ -10,6 +13,7 @@ export default class App extends Component {
       <div>
         <Expansion />
         <CardsList />
+        <Dice />
       </div>
     );
   }
